14ae5e570418467ea54fbee32aa8f098724f5ed1
Vasilii Kuimov <vasilykuimov@yandex.ru>
2024-02-19 20:29:29 +0300

    added entities

---
+++ src/entities/task.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum, auto
+
+
+class TaskStatus(Enum):
+    in_backlog = auto()
+    prioritized = auto()
+    in_work = auto()
+    in_review = auto()
+    done = auto()
+
+
+@dataclass
+class TaskEntity:
+    id: str
+    description: str
+    create_time: datetime
+    update_time: datetime
+    status: TaskStatus

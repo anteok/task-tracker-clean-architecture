74d545d256216dba42ceec4f382316544ad76559
Vasilii Kuimov <vasilykuimov@yandex.ru>
2024-02-25 15:58:41 +0300

    added commands

--- src/views/command.py
+++ src/views/command.py
@@ -1,7 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from typing import Type, TypeVar, Generic
 
-from controllers.in_memory import InMemoryController, CreateTaskController
+from controllers.in_memory import InMemoryController, CreateTaskController, GetTaskDescriptionController, \
+    UpdateStatusController, DeleteTaskController
 from details.id_generator import IdGenerator
 from repositories.in_memory import InMemoryRepository
 
@@ -20,7 +21,7 @@ class CommandPattern(Generic[T], metaclass=ABCMeta):
 
 
 class CreatePattern(CommandPattern[CreateTaskController]):
-    _regex = r'create \"[a-zA-Z\s]*\"'
+    _regex = r'create task \"[a-zA-Z\s]*\"'
     _controller_class = CreateTaskController
 
     def __init__(self, _repository: InMemoryRepository):
@@ -33,3 +34,34 @@ class CreatePattern(CommandPattern[CreateTaskController]):
 
         self._controller.execute(task_id=new_id, description=description)
         return f'A new task "{description}" is in_backlog with id {new_id}'
+
+
+class GetDescriptionPattern(CommandPattern[GetTaskDescriptionController]):
+    _regex = r'what is (\d)*'
+    _controller_class = GetTaskDescriptionController
+
+    def respond(self, command: str) -> str:
+        task_id = command.split()[-1]
+        description = self._controller.execute(task_id)
+        return f'THE TASK WITH ID {task_id} IS "{description}"'
+
+
+class UpdateStatusPattern(CommandPattern[UpdateStatusController]):
+    _regex = r'move task (\d)* to (in_backlog|prioritized|in_work|in_review|done)'
+    _controller_class = UpdateStatusController
+
+    def respond(self, command: str) -> str:
+        task_id = command.split()[2]
+        status = command.split()[-1]
+        self._controller.execute(task_id, status)
+        return f'The task with id {task_id} was moved to status "{status}"'
+
+
+class DeleteTaskPattern(CommandPattern[DeleteTaskController]):
+    _regex = r'delete task (\d)*'
+    _controller_class = DeleteTaskController
+
+    def respond(self, command: str) -> str:
+        task_id = command.split()[-1]
+        self._controller.execute(task_id)
+        return f'The task with id {task_id} was deleted'
--- tests/test_view/test_command_pattern.py
+++ tests/test_view/test_command_pattern.py
@@ -2,18 +2,37 @@ from unittest.mock import Mock, patch
 
 import pytest
 
+from controllers.in_memory import UpdateStatusController, DeleteTaskController
 from details.id_generator import IdGenerator
-from views.command import CreatePattern
+from views.command import CreatePattern, GetDescriptionPattern, UpdateStatusPattern, DeleteTaskPattern
 
 
-class TestCreatePattern:
+class TestCommandPattern:
 
     @patch.object(IdGenerator, 'get_id')
     @pytest.mark.parametrize('command, description', [
-        ('create "new task"', 'new task'),
-        ('create "do something"', 'do something'),
+        ('create task "new task"', 'new task'),
+        ('create task "do something"', 'do something'),
     ])
     def test_create_pattern(self, _id, command, description):
         creator = CreatePattern(Mock())
         creator._id_generator.get_id.return_value = '12'
         assert creator.respond(command) == f'A new task "{description}" is in_backlog with id 12'
+
+    def test_get_description_pattern(self):
+        descriptor = GetDescriptionPattern(
+            Mock(get_task=Mock(return_value=Mock(description='do some')))
+        )
+        assert descriptor.respond('12') == 'THE TASK WITH ID 12 IS "DO SOME"'
+
+    @patch.object(UpdateStatusController, attribute='execute')
+    def test_update_status(self, execute_method):
+        updater = UpdateStatusPattern(Mock())
+        assert updater.respond('move task 12 to prioritized') == 'The task with id 12 was moved to status "prioritized"'
+        execute_method.assert_called_with('12', 'prioritized')
+
+    @patch.object(DeleteTaskController, attribute='execute')
+    def test_delete_task(self, execute_method):
+        deleter = DeleteTaskPattern(Mock())
+        assert deleter.respond('delete task 12') == 'The task with id 12 was deleted'
+        execute_method.assert_called_with('12')
